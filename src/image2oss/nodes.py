7e737103e89bfc97f55ab72a91829cc77ad83ed2
nxt5656 <nxt5656@gmail.com>
2025-04-28 18:35:11 +0800

    支持ak sts服务上传图片

--- src/image2oss/nodes.py
+++ src/image2oss/nodes.py
@@ -3,6 +3,8 @@ from .util import tensor_to_pil,image_to_base64
 from comfy.cli_args import args
 import ast
 from io import BytesIO
+import requests
+
 
 
 OSS_ENDPOINT_LIST = [
@@ -126,11 +128,94 @@ class OSSUploadNode:
         except oss2.exceptions.OssError as e:
             raise ValueError(f'上传失败，错误信息: {e}')
 
+
+class OSSUploadNodeBySTSServiceUrl:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "filename": ("STRING",{"default":'["tmp-comfyui/filename.jpeg"]'}),
+                "sts_service_url": ("STRING", {"default": "https://demo.cn/sts_service"}),
+                "bucket_name": ("STRING", {"default": "bucket_name"}),
+                "endpoint": (OSS_ENDPOINT_LIST,{"default": "oss-cn-hangzhou.aliyuncs.com"}),
+            }
+        }
+        # 校验参数是否正确
+    @classmethod
+    def VALIDATE_INPUTS(cls,image, filename, sts_service_url, bucket_name, endpoint):
+        print("参数校验:\t%s,%s,%s,%s" % (filename, sts_service_url, bucket_name, endpoint))
+        if filename == "" or sts_service_url == "" or bucket_name == "" or endpoint == "":
+            return "参数不能为空"
+        # 检查endpoing
+        if endpoint not in OSS_ENDPOINT_LIST:
+            return "endpoint 不正确\t %s" % endpoint
+        return True
+    RETURN_TYPES = ()
+    FUNCTION = "upload_to_oss"
+    CATEGORY = "API/oss"
+    OUTPUT_NODE = True
+    def upload_to_oss(self, image, filename, sts_service_url, bucket_name, endpoint):
+        #print("参数信息: \t%s,%s,%s,%s,%s\n" %( filename,access_key_id, access_key_secret, bucket_name, endpoint))
+
+        # 先判断这里是不是字符串
+        if not isinstance(filename, str):
+            raise ValueError("文件名必须为列表")
+        try:
+            filenameList = ast.literal_eval(filename)
+            if isinstance(filenameList, list):  # 检查解析后的结果是否是列表
+                pass
+            else:
+                raise ValueError("文件名必须为列表")
+        except:
+            raise ValueError("文件名必须为列表")
+        print("共有图片[%s]张,filename[%s]个\n" % (len(image), len(filenameList) ))
+        if len(image)!= len(filenameList) :
+            raise ValueError("生成图片数量与给定的文件名数量不对应")
+
+        try:
+            response = requests.get(sts_service_url)
+            response.raise_for_status()
+            data = response.json()
+            if not data["success"]:
+                raise ValueError(f"获取ak失败: {data}")
+            access_key_id = data["data"]["accessKeyId"]
+            access_key_secret = data["data"]["accessKeySecret"]
+            security_token = data["data"]["securityToken"]
+        except requests.exceptions.HTTPError as http_err:
+            raise ValueError(f"HTTP 错误发生: {http_err}")
+        except requests.exceptions.RequestException as req_err:
+            raise ValueError(f"请求错误发生: {req_err}")
+        except ValueError as value_err:
+            raise ValueError(f"JSON 解析错误: {value_err}")
+        n = 0
+        for i in image:
+            img = tensor_to_pil(i)
+            self.put_object(img,filenameList[n],access_key_id, access_key_secret, security_token,bucket_name, endpoint)
+            n = n + 1
+
+        return ()
+    def put_object(self,file,filename,access_key_id, access_key_secret, security_token,bucket_name, endpoint):
+        auth = oss2.StsAuth(access_key_id,access_key_secret,security_token,auth_version = "v2")
+        bucket = oss2.Bucket(auth, endpoint, bucket_name)
+        image_bytes = BytesIO()
+        file.save(image_bytes, format='JPEG')  # 保存为 JPEG 格式
+        image_bytes.seek(0)  # 将流指针回到开头
+        try:
+            bucket.put_object(filename, image_bytes)
+            print(f'图片成功上传到 OSS，文件名为: {filename}')
+        except oss2.exceptions.OssError as e:
+            raise ValueError(f'上传失败，错误信息: {e}')
+
+
+
 NODE_CLASS_MAPPINGS = {
-    "OSSUploadNode": OSSUploadNode
+    "OSSUploadNode": OSSUploadNode,
+    "OSSUploadNodeBySTSServiceUrl": OSSUploadNodeBySTSServiceUrl
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "OSSUploadNode": "保存图片到oss上"
+    "OSSUploadNode": "保存图片到oss上",
+    "OSSUploadNodeBySTSServiceUrl": "保存图片到oss上(使用服务获取ak信息)"
 }
